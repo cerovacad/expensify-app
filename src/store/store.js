444891bbb55b9cd217e4470a7003e47904bbd255
Cerovac <cerovacad@gmail.com>
2018-03-24 16:09:45 +0100

    database setup

--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,20 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import expensesReducer from '../reducers/expensesReducer';
 import filtersReducer from '../reducers/filtersReducer';
 import authReducer from '../reducers/authReducer';
 
-const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
+  const store = createStore(
+    combineReducers({
+      expenses: expensesReducer,
+      filters: filtersReducer,
+      auth: authReducer
+    }),
+    composeEnhancers(applyMiddleware(thunk))
+  );
 
-    const store = createStore(
-        combineReducers({
-            expenses: expensesReducer,
-            filters: filtersReducer,
-            auth: authReducer
-        }),
-        composeEnchancers(applyMiddleware(thunk))
-        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
-
-    return store;
+  return store;
 };
